874974cb6b3f82aafa96f4c5dd97c666285bf010
matinsaravanifr <saravanimatin6@gmail.com>
2025-12-28 10:34:26 -0800

    update hero section

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -2,7 +2,7 @@ import hero from "./../../assets/Image.png";
 
 export default function HeroSection() {
   return (
-    <section className="w-[80%] m-auto mb-10">
+    <section className="hidden lg:block lg:w-[80%] lg:m-auto lg:mb-10 ">
       <img src={hero} alt="hero" />
     </section>
   );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import Header from'./../../Components/Header/Header'
 import HeroSection from './../../Components/HeroSection/HeroSection'
 import Articles from './../../Components/Articles/Articles'
 import Footer from './../../Components/Footer/Footer'
-import Spinner from '../../Components/Spinner/Spinner'
 export default function Home() {
   return (
     <>
