520aaf01a680d2dc24076741306d6875127a50ed
matinsaravanifr <saravanimatin6@gmail.com>
2025-12-29 15:17:35 -0800

    create About Us page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,13 +3,15 @@ import HeroSection from './../../Components/HeroSection/HeroSection'
 import Articles from './../../Components/Articles/Articles'
 import Footer from './../../Components/Footer/Footer'
 import Article_page from '../Article_page/Article_page'
+import About_Us from '../About_Us/About_Us'
 export default function Home() {
   return (
     <>
-  <Header/>
+  {/* <Header/>
   <HeroSection/>
   <Articles/>
-  <Footer/>
+  <Footer/> */}
+  <About_Us/>
     </>
   )
 }
