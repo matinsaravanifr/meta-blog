212f19306064ea97574d3462c8d02fc3d3485af9
matinsaravanifr <saravanimatin6@gmail.com>
2025-12-29 10:41:46 -0800

    update mobile menu Ui

--- src/Components/Header/Mobil_Ui.jsx
+++ src/Components/Header/Mobil_Ui.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars } from "react-icons/fa";
 
 export default function Mobil_Ui() {
+  const[isopen,setisopen]=useState(false)
   return (
 
-    <header className='z-50 relative flex justify-end'>
-    <nav className='flex flex-col fixed w-[50%] h-full bg-gray-600'>
+    <header className='z-50 relative flex justify-end lg:hidden'>
+        <button className='mt-4 mb-10 mr-10' onClick={()=>setisopen(true)}> 
+          <FaBars className='' />
 
-        <button className='mt-3 ml-2 mb-4 ' >
+        </button>
+    <nav className={`flex flex-col top-0 right-0 fixed w-[50%] h-full transform transition-transform  delay-100 duration-300 ease-in-out bg-gray-300 ${isopen ? "translate-x-0" : "translate-x-full"}`}>
+        <button className='mt-3 ml-2 mb-4 cursor-pointer 'onClick={()=>{setisopen(false)}} >
             <FaBars className='w-5 h-10' />
         </button>
     <ul  className='text-center'>  
 
-    <li className='pb-4'>Home</li>
-    <li className='pb-4'>Bloge</li>
-    <li className='pb-4'>About Us</li>
-    <li className='pb-4'>contact</li>
+    <li className=" pb-4 cursor-pointer">
+      Home
+      </li>
+    <li className='pb-4 cursor-pointer'>
+      <a href="">
+      Bloge
+      </a>
+      </li>
+    <li className='pb-4 cursor-pointer'>
+      <a href="">
+
+      About Us
+      </a>
+      </li>
+    <li className='pb-4 cursor-pointer'>
+      <a href="">
+
+      contact
+      </a>
+      </li>
 
     </ul>
 
